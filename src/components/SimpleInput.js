e71f8982e5daf92902bdfa5ee220d7bed1196910
bondanjody <bondanjodys@gmail.com>
2023-03-22 13:26:15 +0700

    Menggunakan prinsip 'was touched' pada tiap field form. Menambahkan state 'touched' untuk mengatur field yang 'disentuh/touched', serta menggunakannya untuk mengatur conditional styling.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,16 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Entered name is valid.");
+    }
+  }, [enteredNameIsValid])
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -11,6 +18,8 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = event => {
     event.preventDefault();
+
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === '') {
       setEnteredNameIsValid(false);
@@ -26,14 +35,15 @@ const SimpleInput = (props) => {
 
   }
 
-  const nameInputStyle = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputStyle = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputStyle}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
-        {!enteredNameIsValid && <p className="error-text">Name must not be EMPTY !</p>}
+        {nameInputIsInvalid && <p className="error-text">Name must not be EMPTY !</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
